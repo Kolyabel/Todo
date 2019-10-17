86260b173107b5e520d160dee2ffe941561477bf
Kolyabel <krchan@yandex.ru>
2019-10-17 22:34:52 +0300

    work

--- src/InputData.js
+++ src/InputData.js
@@ -22,7 +22,7 @@ class InputData extends React.PureComponent {
             console.log("valueInput:",event.target.value)
                     
             const newState = {
-                list:[].push(...this.state.list, this.state.value)
+                list:[].push(...this.state.list, this.state.value),
                 value: ""
             }
 
