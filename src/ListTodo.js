2d57eb056bc7592e2ac47f0f500aee9c63277bdd
Kolyabel <krchan@yandex.ru>
2019-10-21 12:52:26 +0300

    work

--- src/ListTodo.js
+++ src/ListTodo.js
@@ -2,14 +2,54 @@ import React from 'react';
 import App from "./App";
 
 class ListTodo extends React.PureComponent {
+
+    constructor(props) {
+        super(props)
+
+        const { list = []} = props
+        this.state = {list}
+
+        console.log("listState",this.state.list)
+    }
+
+    // state = {
+    //     check: false,
+    //     listState: this.props
+    // }
+
+    handleChange = (event) => {
+        console.log("checkbox",event.target.checked,event.target)
+    }
+
+    buttonActive = () =>{
+    }
+
+    buttonCompleted = () =>{
+    }
+
+    buttonAll = () =>{
+    }
+
     render() {
         const { list = []} = this.props
+        console.log("this.props",this.props, this.props.list)
+        console.log("listState",this.state.list)
         return (
-            <ul>
-                { list.map( (item, index) => (
-                    <li key={index}>{item}</li>
-                ))}
-            </ul>
+            <div>
+                <ul>
+                    { list.map( (item) => (
+                        <li key={item.id}>
+                            <input type="checkbox"
+                                   onChange={this.handleChange}/>
+                            {item.value}
+                        </li>
+                    ))}
+                </ul>
+                <button onClick={this.buttonActive}> Отмеченные </button>
+                <button onClick={this.buttonCompleted}> Завершенные </button>
+                <button onClick={this.buttonAll}> Все </button>
+            </div>
+
         )
     }
 }
