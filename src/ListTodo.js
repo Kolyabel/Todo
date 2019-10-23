701157a7ec27db0530ec14239e3452f35aba00d0
Kolyabel <krchan@yandex.ru>
2019-10-23 09:11:24 +0300

    work

--- src/ListTodo.js
+++ src/ListTodo.js
@@ -24,7 +24,6 @@ class ListTodo extends React.PureComponent {
 
     render() {
         const { list = []} = this.props
-        const activeSort = "all"
         let listFilter = this.props.list
 
         if (this.state.sortList == "active"){
